5ca08c78cb4fa675b83563b4aeabffbd5aa593cc
lenarddome <lenarddome@protonmail.ch>
2022-07-21 14:48:20 +0100

    add gdistance

--- pkg/src/gdistance.cpp
+++ pkg/src/gdistance.cpp
@@ -1,7 +1,6 @@
 // Copyright 2022 <Lenard Dome> [legal/copyright]
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
-#include <fstream>
 
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
@@ -10,28 +9,29 @@ using namespace arma;
 // accomodation statistics
 // compare two cubes of inequality matrices
 // returns the complete list of unique ordinal matrices
-vec alpha(cube discovered, cube predicted) {
-  vec accomodated(discovered.n_slices, fill::zeros);
-  mat index(predicted.n_slices, discovered.n_slices);
+colvec accomodation(cube discovered, cube predicted) {
+  colvec accomodated(discovered.n_slices, fill::zeros);
+  mat index(discovered.n_slices, predicted.n_slices);
   // carry out the comparisons
-  for (int x = 0; x < predicted.n_slices; x++) {
-    mat current = predicted.slice(x);
-    for (int y = 0; y < discovered.n_slices; y++) {
-      mat base = discovered.slice(y);
+  for (int x = 0; x < discovered.n_slices; x++) {
+    mat current = discovered.slice(x);
+    for (int y = 0; y < predicted.n_slices; y++) {
+      mat base = predicted.slice(y);
       umat result = (base == current);
       uvec comparisons = result(trimatu_ind(size(result), 1));
-      index(x, y) =  any(comparisons == 0);
+      index(x, y) =  all(comparisons == 1);
     }
     // if matrix has been found, set value to 1
-    if (all(index.row(x) == 1)) {
+    if (any(index.row(x) == 1)) {
       accomodated(x) = 1;
     }
   }
-  return(accomodated);
+  colvec out = conv_to< colvec >::from(accomodated);
+  return(out);
 }
 
 // prediction statistics
-double beta(cube discovered, cube predicted) {
+double prediction(cube discovered, cube predicted) {
   double prediction = 0;
   mat index(predicted.n_slices, discovered.n_slices);
   // carry out the comparisons
@@ -41,24 +41,35 @@ double beta(cube discovered, cube predicted) {
       mat base = discovered.slice(y);
       umat result = (base == current);
       uvec comparisons = result(trimatu_ind(size(result), 1));
-      index(x, y) =  any(comparisons == 0);
+      index(x, y) =  all(comparisons == 0);
     }
     // if matrix has been found, set value to 1
-    if (all(index.row(x) == 1)) {
+    if (all(index.row(x) == 0)) {
       prediction += 1;
     }
   }
   return(prediction);
 }
 
-// weighting accomodation
-double alphaWeighted(vec accomodated, vec frequencies) {
-  return(alpha_weighted)
-}
-
 // weighted euclidean distance
 
 // [[Rcpp::export]]
-List gdistance(cube human, cube model, double universal, double weights, vec frequencies, bool xtdo = false) {
-
+List gdistance(arma::cube human, arma::cube model, double universal, double weight,
+               arma::colvec frequencies,
+              bool xtdo = false) {
+  List out;
+  vec alpha = accomodation(human, model);
+  double alpha_weighted = sum(alpha % frequencies);
+  double beta = prediction(human, model) / universal;
+  double distance = sqrt(weight * pow(1 - alpha_weighted, 2) +  (1 - weight) * pow(0 - beta, 2));
+  if (xtdo) {
+    out = Rcpp::List::create(
+      Rcpp::Named("gdistance") = distance,
+      Rcpp::Named("alpha") = alpha_weighted,
+      Rcpp::Named("beta") = beta);
+  } else {
+    out = Rcpp::List::create(
+      Rcpp::Named("gdistance") = distance);
+  }
+  return(out);
 }
