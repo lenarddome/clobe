dde7df51fa6a17795c54265e93db89111a0fb770
lenarddome <lenarddome@gmail.com>
2022-10-19 12:19:46 +0100

    compile attributes for R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,14 +2,14 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 brutes <- function(probabilities, length, thresholds) {
-    .Call(`_globe_brutes`, probabilities, length, thresholds)
+    .Call(`_clobe_brutes`, probabilities, length, thresholds)
 }
 
 gdistance <- function(human, model, universal, weight, frequencies, xtdo = FALSE) {
-    .Call(`_globe_gdistance`, human, model, universal, weight, frequencies, xtdo)
+    .Call(`_clobe_gdistance`, human, model, universal, weight, frequencies, xtdo)
 }
 
 imac <- function(probabilities, thresholds) {
-    .Call(`_globe_imac`, probabilities, thresholds)
+    .Call(`_clobe_imac`, probabilities, thresholds)
 }
 
